70bbadcb996357903ae9f772e509b2b14117f103
Lotfi Arif <lotfi@ninetailed.com>
2024-06-22 19:57:37 +0700

    chore: Add Prisma service to modules

--- src/supervisors/supervisors.module.ts
+++ src/supervisors/supervisors.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SupervisorsService } from './supervisors.service';
 import { SupervisorsController } from './supervisors.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [SupervisorsController],
-  providers: [SupervisorsService],
+  providers: [SupervisorsService, PrismaService],
 })
 export class SupervisorsModule {}
