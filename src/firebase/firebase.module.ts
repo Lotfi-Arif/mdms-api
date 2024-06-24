c7fb47a19bcd7ee5a2fdf230395c2ae24ba59ea0
Lotfi Arif <lotfi@ninetailed.com>
2024-06-25 01:27:12 +0700

    chore: Update database URL in .env.template

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -11,19 +11,18 @@ const firebaseProvider = {
   useFactory: (configService: ConfigService) => {
     const firebaseConfig = {
       type: configService.get<string>('TYPE'),
-      project_id: configService.get<string>('PROJECT_ID'),
-      private_key_id: configService.get<string>('PRIVATE_KEY_ID'),
-      private_key: configService.get<string>('PRIVATE_KEY'),
-      client_email: configService.get<string>('CLIENT_EMAIL'),
-      client_id: configService.get<string>('CLIENT_ID'),
-      auth_uri: configService.get<string>('AUTH_URI'),
-      token_uri: configService.get<string>('TOKEN_URI'),
-      auth_provider_x509_cert_url: configService.get<string>('AUTH_CERT_URL'),
-      client_x509_cert_url: configService.get<string>('CLIENT_CERT_URL'),
-      universe_domain: configService.get<string>('UNIVERSAL_DOMAIN'),
-    } as admin.ServiceAccount;
+      projectId: configService.get<string>('PROJECT_ID'),
+      privateKeyId: configService.get<string>('PRIVATE_KEY_ID'),
+      privateKey: configService.get<string>('PRIVATE_KEY'),
+      clientEmail: configService.get<string>('CLIENT_EMAIL'),
+      clientId: configService.get<string>('CLIENT_ID'),
+      authUri: configService.get<string>('AUTH_URI'),
+      tokenUri: configService.get<string>('TOKEN_URI'),
+      authProviderX509CertUrl: configService.get<string>('AUTH_CERT_URL'),
+      clientC509CertUrl: configService.get<string>('CLIENT_CERT_URL'),
+      universeDomain: configService.get<string>('UNIVERSAL_DOMAIN'),
+    };
 
-    // Log the values of the config for debugging purposes
     logger.log(`Firebase Config: ${JSON.stringify(firebaseConfig)}`);
 
     if (!firebaseConfig.privateKey) {
