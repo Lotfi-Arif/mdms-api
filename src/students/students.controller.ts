f0bc281d9f26cce529ddafc77d8c9e0d01460dab
Lotfi Arif <lotfi@ninetailed.com>
2024-06-24 22:34:23 +0700

    chore: Update StudentsController with logging and error handling for getStudentById, getAllStudents, getStudentProgress, addStudentSubmission, deleteStudentSubmission, getLecturerList, and getVivaDetails methods

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,31 +1,63 @@
-import { Controller, Post, Body, Param, Get, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Get,
+  Delete,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { Lecturer, Project, Student, Submission, Viva } from '@prisma/client';
 
 @Controller('students')
 export class StudentsController {
+  private readonly logger = new Logger(StudentsController.name);
   constructor(private readonly studentsService: StudentsService) {}
 
   @Get(':id')
-  getStudentById(@Param('id') studentId: string): Promise<Student | null> {
-    return this.studentsService.getStudentById(studentId);
+  async getStudentById(
+    @Param('id') studentId: string,
+  ): Promise<Student | null> {
+    this.logger.debug(`Fetching student with ID: ${studentId}`);
+    const student = await this.studentsService.getStudentById(studentId);
+    this.logger.debug(`Fetched student: ${JSON.stringify(student)}`);
+
+    if (!student) {
+      this.logger.error(`Student with ID: ${studentId} not found`);
+      throw new NotFoundException('Student does not exist');
+    }
+
+    return student;
   }
 
   @Get()
-  getAllStudents(): Promise<Student[]> {
-    return this.studentsService.getAllStudents();
+  async getAllStudents(): Promise<Student[]> {
+    this.logger.debug('Fetching all students');
+    const students = await this.studentsService.getAllStudents();
+    this.logger.debug(`Fetched ${students.length} students`);
+
+    return students;
   }
 
   @Get(':id/progress')
-  getStudentProgress(@Param('id') studentId: string): Promise<{
+  async getStudentProgress(@Param('id') studentId: string): Promise<{
     student: Student;
     progress: number;
   }> {
-    return this.studentsService.getStudentProgress(studentId);
+    this.logger.debug(`Fetching progress for student with ID: ${studentId}`);
+    const progress = await this.studentsService.getStudentProgress(studentId);
+    this.logger.debug(`Fetched progress: ${JSON.stringify(progress)}`);
+
+    return {
+      student: progress.student,
+      progress: progress.progress,
+    };
   }
 
   @Post(':id/submissions')
-  addStudentSubmission(
+  async addStudentSubmission(
     @Param('id') studentId: string,
     @Body('title') title: string,
     @Body('content') content: string,
@@ -33,28 +65,57 @@ export class StudentsController {
     message: string;
     submission: Submission;
   }> {
-    return this.studentsService.addStudentSubmission(studentId, title, content);
+    this.logger.debug(
+      `Adding submission for student with ID: ${studentId} with title: ${title}`,
+    );
+    const submission = await this.studentsService.addStudentSubmission(
+      studentId,
+      title,
+      content,
+    );
+    this.logger.debug(`Added submission: ${JSON.stringify(submission)}`);
+
+    return submission;
   }
 
   @Delete(':id')
-  deleteStudentSubmission(@Param('id') submissionId: string): Promise<{
+  async deleteStudentSubmission(@Param('id') submissionId: string): Promise<{
     message: string;
   }> {
-    return this.studentsService.deleteStudentSubmission(submissionId);
+    this.logger.debug(`Deleting submission with ID: ${submissionId}`);
+    const message =
+      await this.studentsService.deleteStudentSubmission(submissionId);
+    this.logger.debug(`Deleted submission: ${JSON.stringify(message)}`);
+
+    return message;
   }
 
   @Get('lecturers')
-  getLecturerList(): Promise<Lecturer[]> {
-    return this.studentsService.getLecturerList();
+  async getLecturerList(): Promise<Lecturer[]> {
+    this.logger.debug('Fetching all lecturers');
+    const lecturers = await this.studentsService.getLecturerList();
+    this.logger.debug(`Fetched ${lecturers.length} lecturers`);
+
+    return lecturers;
   }
 
   @Get('projects')
-  getProjectArchive(): Promise<Project[]> {
-    return this.studentsService.getProjectArchive();
+  async getProjectArchive(): Promise<Project[]> {
+    this.logger.debug('Fetching all projects');
+    const projects = await this.studentsService.getProjectArchive();
+    this.logger.debug(`Fetched ${projects.length} projects`);
+
+    return projects;
   }
 
   @Get(':id/viva-details')
-  getVivaDetails(@Param('id') studentId: string): Promise<Viva | null> {
-    return this.studentsService.getVivaDetails(studentId);
+  async getVivaDetails(@Param('id') studentId: string): Promise<Viva | null> {
+    this.logger.debug(
+      `Fetching viva details for student with ID: ${studentId}`,
+    );
+    const viva = await this.studentsService.getVivaDetails(studentId);
+    this.logger.debug(`Fetched viva details: ${JSON.stringify(viva)}`);
+
+    return viva;
   }
 }
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 import {
   Student,
@@ -11,28 +11,114 @@ import {
 
 @Injectable()
 export class StudentsService {
+  private readonly logger = new Logger(StudentsService.name);
+
   constructor(private prisma: PrismaService) {}
 
   async createStudent(data: Prisma.StudentCreateInput): Promise<Student> {
-    return this.prisma.student.create({ data });
+    this.logger.debug(
+      `Creating new student with data: ${JSON.stringify(data)}`,
+    );
+    const student = await this.prisma.student.create({ data });
+    this.logger.debug(`Created new student with ID: ${student.id}`);
+
+    return student;
   }
 
   async getAllStudents(): Promise<Student[]> {
-    return this.prisma.student.findMany();
+    this.logger.debug('Fetching all students');
+    const students = await this.prisma.student.findMany({
+      include: {
+        user: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+        supervisor: {
+          include: {
+            lecturer: {
+              select: {
+                user: {
+                  select: {
+                    name: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+    this.logger.debug(`Fetched ${students.length} students`);
+
+    return students;
   }
 
   async getStudentById(id: string): Promise<Student | null> {
-    return this.prisma.student.findUnique({ where: { id } });
+    this.logger.debug(`Fetching student with ID: ${id}`);
+    const student = await this.prisma.student.findUnique({
+      where: { id },
+      include: {
+        user: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+        supervisor: {
+          include: {
+            lecturer: {
+              select: {
+                user: {
+                  select: {
+                    name: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+    this.logger.debug(`Fetched student: ${JSON.stringify(student)}`);
+
+    return student;
   }
 
   async updateStudent(
     id: string,
     data: Prisma.StudentUpdateInput,
   ): Promise<Student> {
-    return this.prisma.student.update({
+    this.logger.debug(`Updating student with ID: ${id}`);
+    const updated = await this.prisma.student.update({
       where: { id },
       data,
+      include: {
+        user: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+        supervisor: {
+          include: {
+            lecturer: {
+              select: {
+                user: {
+                  select: {
+                    name: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
     });
+    this.logger.debug(`Updated student: ${JSON.stringify(updated)}`);
+
+    return updated;
   }
 
   async deleteStudent(id: string): Promise<Student> {
@@ -44,6 +130,7 @@ export class StudentsService {
     student: Student;
     progress: number;
   }> {
+    this.logger.debug(`Fetching student progress with ID: ${studentId}`);
     const student = await this.prisma.student.findUnique({
       where: { id: studentId },
       include: {
@@ -68,22 +155,33 @@ export class StudentsService {
         },
       },
     });
+    this.logger.debug(`Fetched student: ${JSON.stringify(student)}`);
 
     if (!student) {
+      this.logger.error(`Student with ID ${studentId} not found`);
       throw new BadRequestException('Student not found.');
     }
 
+    this.logger.debug(`Fetching submissions for student with ID: ${studentId}`);
     const submissions = await this.prisma.submission.findMany({
       where: { studentId },
     });
+    this.logger.debug(
+      `Fetched ${submissions.length} submissions for student with ID: ${studentId}`,
+    );
 
     const totalPhases = 4;
     const completedPhases = submissions.length;
     if (submissions.length > totalPhases) {
+      this.logger.error(
+        `Student with ID ${studentId} has more than 4 submissions`,
+      );
       throw new BadRequestException(
         'A student cannot create more than 4 submissions.',
       );
     }
+
+    this.logger.debug(`Calculating progress for student with ID: ${studentId}`);
 
     return {
       student,
@@ -100,11 +198,20 @@ export class StudentsService {
     message: string;
     submission: Submission;
   }> {
+    this.logger.debug(
+      `Creating new submission for student with ID: ${studentId}`,
+    );
     const submissions = await this.prisma.submission.findMany({
       where: { studentId },
     });
+    this.logger.debug(
+      `Fetched ${submissions.length} submissions for student with ID: ${studentId}`,
+    );
 
     if (submissions.length >= 4) {
+      this.logger.error(
+        `Student with ID ${studentId} has more than 4 submissions`,
+      );
       throw new BadRequestException(
         'A student cannot create more than 4 submissions.',
       );
@@ -120,6 +227,10 @@ export class StudentsService {
       },
     });
 
+    this.logger.debug(
+      `Created new submission with ID: ${submission.id} for student with ID: ${studentId}`,
+    );
+
     return {
       message: 'Submission created successfully.',
       submission,
@@ -130,23 +241,28 @@ export class StudentsService {
   async deleteStudentSubmission(
     submissionId: string,
   ): Promise<{ message: string }> {
+    this.logger.debug(`Deleting submission with ID: ${submissionId}`);
     const submission = await this.prisma.submission.findUnique({
       where: { id: submissionId },
     });
 
     if (!submission) {
+      this.logger.error(`Submission with ID ${submissionId} not found`);
       throw new BadRequestException('Submission not found.');
     }
 
+    this.logger.debug(`Deleting submission with ID: ${submissionId}`);
     await this.prisma.submission.delete({
       where: { id: submissionId },
     });
+    this.logger.debug(`Deleted submission with ID: ${submissionId}`);
 
     return { message: `Submission with ID ${submissionId} has been deleted.` };
   }
 
   // Displays all the lecturers that are registered on the system
   async getLecturerList(): Promise<Lecturer[]> {
+    this.logger.debug('Fetching all lecturers');
     const lecturers = await this.prisma.lecturer.findMany({
       include: {
         user: {
@@ -157,11 +273,14 @@ export class StudentsService {
         },
       },
     });
+    this.logger.debug(`Fetched ${lecturers.length} lecturers`);
+
     return lecturers;
   }
 
   // Displays all the projects that students have submitted
   async getProjectArchive(): Promise<Project[]> {
+    this.logger.debug('Fetching all projects');
     const projects = await this.prisma.project.findMany({
       include: {
         student: {
@@ -176,12 +295,16 @@ export class StudentsService {
         },
       },
     });
+    this.logger.debug(`Fetched ${projects.length} projects`);
 
     return projects;
   }
 
   // Displays the project details of a specific student
   async getVivaDetails(studentId: string): Promise<Viva | null> {
+    this.logger.debug(
+      `Fetching viva details for student with ID: ${studentId}`,
+    );
     const viva = await this.prisma.viva.findFirst({
       where: { studentId },
       include: {
@@ -205,8 +328,12 @@ export class StudentsService {
         },
       },
     });
+    this.logger.debug(`Fetched viva details for student with ID: ${studentId}`);
 
     if (!viva) {
+      this.logger.error(
+        `Viva details not found for student with ID ${studentId}`,
+      );
       return null;
     }
 
