66928d2bb594065e01c844959280db3fe245c44b
Lotfi Arif <lotfi@ninetailed.com>
2024-06-25 13:32:48 +0700

    chore: Refactor auth controller to use token instead of idToken for login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,19 @@
-import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
+// src/auth/auth.controller.ts
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { FirebaseAuthGuard } from './firebase-auth.guard';
-import { FirebaseUser, LoginResponse, RegisterUserDto } from './auth.types';
+import { RegisterUserDto } from './auth.types';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('login')
-  async login(@Body('idToken') idToken: string): Promise<LoginResponse> {
-    const user = await this.authService.validateFirebaseToken(idToken);
-    return this.authService.login(user);
-  }
-
   @Post('register')
-  async register(
-    @Body() registerUserDto: RegisterUserDto,
-  ): Promise<LoginResponse> {
+  async register(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.register(registerUserDto);
   }
 
-  @UseGuards(FirebaseAuthGuard)
-  @Post('protected')
-  getProtectedData(@Request() req): FirebaseUser {
-    return req.user;
+  @Post('login')
+  async login(@Body() tokenId: string) {
+    return this.authService.login(tokenId);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,21 @@
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+// src/auth/auth.service.ts
+import {
+  Injectable,
+  UnauthorizedException,
+  Logger,
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { FirebaseService } from '../firebase/firebase.service';
 import { PrismaService } from 'nestjs-prisma';
+import * as bcrypt from 'bcrypt';
+import * as admin from 'firebase-admin';
 import {
   JwtPayload,
   LoginResponse,
-  FirebaseUser,
   RegisterUserDto,
+  FirebaseUser,
 } from './auth.types';
-import * as bcrypt from 'bcrypt';
-import * as admin from 'firebase-admin';
 
 @Injectable()
 export class AuthService {
@@ -17,112 +23,137 @@ export class AuthService {
 
   constructor(
     private readonly jwtService: JwtService,
-    private readonly firebaseService: FirebaseService,
     private readonly prisma: PrismaService,
   ) {}
 
   async validateFirebaseToken(idToken: string): Promise<FirebaseUser> {
     try {
-      this.logger.debug('Validating Firebase token');
       const decodedToken = await admin.auth().verifyIdToken(idToken);
-      this.logger.debug('Decoded Firebase token', decodedToken);
-      const user = await this.prisma.user.findUnique({
-        where: { email: decodedToken.email },
-        select: { id: true, email: true },
-      });
-      this.logger.debug('User found', user);
-
-      if (!user) {
-        this.logger.error('User not found');
-        throw new UnauthorizedException('User not found');
-      }
-
       return {
-        uid: user.id,
-        email: user.email,
-        email_verified: true,
+        email_verified: decodedToken.email_verified,
+        uid: decodedToken.uid,
+        email: decodedToken.email,
+        name: decodedToken.name,
       };
     } catch (error) {
-      this.logger.error('Invalid token', error);
-      throw new UnauthorizedException('Invalid token');
+      this.logger.error('Error validating Firebase token', error);
+      throw new UnauthorizedException('Invalid Firebase token');
     }
   }
 
-  async login(user: FirebaseUser): Promise<LoginResponse> {
-    this.logger.debug('Logging in user', user);
-    const payload: JwtPayload = { username: user.email, sub: user.uid };
-    this.logger.debug('Generated JWT payload', payload);
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
-  }
-
   async register(registerUserDto: RegisterUserDto): Promise<LoginResponse> {
-    this.logger.debug('Registering user', registerUserDto);
+    const { email, password, name } = registerUserDto;
 
-    // Create user in Firebase
-    const firebaseUser = await admin.auth().createUser({
-      email: registerUserDto.email,
-      password: registerUserDto.password,
-      displayName: registerUserDto.name,
-    });
+    // First, hash the password for local storage
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    this.logger.debug('Firebase user created', firebaseUser);
+    // Then, prepare the user data for local storage (temporarily hold it)
+    const userData = {
+      email,
+      password: hashedPassword,
+      name,
+      uid: null, // Placeholder for Firebase UID, to be updated
+    };
 
-    // Hash the password for local storage
-    const hashedPassword = await bcrypt.hash(registerUserDto.password, 10);
+    // Check if the email already exists in Firebase
+    try {
+      const existingUser = await admin.auth().getUserByEmail(email);
+      if (existingUser) {
+        throw new ConflictException(
+          'The email address is already in use by another account.',
+        );
+      }
+    } catch (error) {
+      if (error.code !== 'auth/user-not-found') {
+        this.logger.error('Failed to check user existence in Firebase', error);
+        throw new InternalServerErrorException(
+          'Failed to check user existence.',
+        );
+      }
+    }
 
-    // Determine role and create corresponding records
+    // Now attempt to store user in the local database before creating in Firebase
     let user;
-    if (registerUserDto.role === 'lecturer') {
-      this.logger.debug('Registering lecturer');
+    try {
       user = await this.prisma.user.create({
         data: {
-          email: registerUserDto.email,
-          name: registerUserDto.name,
-          password: hashedPassword,
-          uid: firebaseUser.uid,
-          emailVerified: firebaseUser.emailVerified,
-          displayName: firebaseUser.displayName,
-          photoURL: firebaseUser.photoURL,
-          phoneNumber: firebaseUser.phoneNumber,
-          disabled: firebaseUser.disabled,
-          lecturer: {
+          ...userData,
+          [registerUserDto.role]: {
             create: {
-              staffNumber: registerUserDto.universityId,
+              ...(registerUserDto.role === 'lecturer'
+                ? { staffNumber: registerUserDto.universityId }
+                : { matricNumber: registerUserDto.universityId }),
             },
           },
         },
       });
-
-      this.logger.debug('Lecturer created', user);
+    } catch (dbError) {
+      this.logger.error('Failed to create user in the local database', dbError);
+      throw new InternalServerErrorException('Failed to create user locally.');
     }
-    // Register student if role is not specified or is student
-    this.logger.debug('Registering student');
-    user = await this.prisma.user.create({
-      data: {
-        email: registerUserDto.email,
-        name: registerUserDto.name,
-        password: hashedPassword,
-        uid: firebaseUser.uid,
-        emailVerified: firebaseUser.emailVerified,
-        displayName: firebaseUser.displayName,
-        photoURL: firebaseUser.photoURL,
-        phoneNumber: firebaseUser.phoneNumber,
-        disabled: firebaseUser.disabled,
-        student: {
-          create: {
-            matricNumber: registerUserDto.universityId,
-          },
-        },
-      },
-    });
 
-    this.logger.debug('Student created', user);
+    // If local db creation was successful, proceed to create user in Firebase
+    try {
+      const firebaseUser = await admin.auth().createUser({
+        email,
+        password,
+      });
+      // Update the local database with Firebase UID
+      user = await this.prisma.user.update({
+        where: { id: user.id },
+        data: { uid: firebaseUser.uid },
+      });
+    } catch (firebaseError) {
+      // If Firebase user creation fails, optionally clean up the local database
+      await this.prisma.user.delete({ where: { id: user.id } });
+      this.logger.error('Failed to create user in Firebase', firebaseError);
+      throw new InternalServerErrorException(
+        'Failed to create user in Firebase.',
+      );
+    }
 
-    const payload: JwtPayload = { username: user.email, sub: user.id };
+    // Generate JWT token
+    const payload: JwtPayload = { username: user.email, sub: user.uid };
     return {
       access_token: this.jwtService.sign(payload),
     };
+  }
+
+  async login(idToken: string): Promise<LoginResponse> {
+    try {
+      // Verify the ID token directly with Firebase Admin SDK
+      const decodedToken = await admin.auth().verifyIdToken(idToken);
+      const uid = decodedToken.uid;
+
+      // Optionally, retrieve additional user details from your local database
+      const user = await this.prisma.user.findUnique({
+        where: {
+          uid,
+          id: uid,
+        },
+        select: { email: true, uid: true }, // Customize based on stored user data
+      });
+
+      if (!user) {
+        throw new UnauthorizedException('User not found in local database');
+      }
+
+      // Generate JWT token for session management in your application
+      const payload: JwtPayload = { username: user.email, sub: user.uid };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    } catch (error) {
+      // Handle errors from Firebase or local database issues
+      if (
+        error.code === 'auth/id-token-expired' ||
+        error.code === 'auth/id-token-revoked'
+      ) {
+        throw new UnauthorizedException('Session expired');
+      } else {
+        console.error('Authentication error:', error);
+        throw new UnauthorizedException('Authentication failed');
+      }
+    }
   }
 }
